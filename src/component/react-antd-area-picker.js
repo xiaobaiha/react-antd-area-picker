44bc789bcd0eec8ba9af61a74c2630dc54cc29b6
xiaobaiha <xiaobaihaha0001@163.com>
2018-06-17 00:18:46 +0800

    @dev: basic framework

---
+++ src/component/react-antd-area-picker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './react-antd-area-picker.css';
+
+class AreaPicker extends React.Component{
+  render(){
+    return ("Hello npm!");
+  }
+}
+
+module.exports = AreaPicker;
