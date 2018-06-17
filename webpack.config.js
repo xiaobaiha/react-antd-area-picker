a7600cd1fe9eb6f9e26855bfc7db6f82f4c38228
xiaobaiha <xiaobaihaha0001@163.com>
2018-06-17 12:56:37 +0800

    @dev: able to cite

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,12 @@ module.exports = {
   module: {
     rules: [
       { 
-        test: /\.js$/, 
+        test: /\.jsx?$/, 
         exclude: /node_modules/, 
-        loader: 'babel-loader?presets[]=env&presets[]=react' 
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        } 
       },
       { 
         test: /\.css$/, 
